6e0339f3dc885c13a4d292b78933c219942653b7
cedric <cedric.fortuin@unitracer.nl>
2022-04-28 20:10:52 +0200

    fuck

--- src/App.js
+++ src/App.js
@@ -3,16 +3,15 @@ import React, {useEffect} from 'react';
 export default function Quote() {
     useEffect(() => {
         const quotes = require('../quotes.json');
+        document.querySelector('#quote').innerHTML = quotes[Math.floor(Math.random() * quotes.length)];
         setInterval(() => {
             document.querySelector('#quote').innerHTML = quotes[Math.floor(Math.random() * quotes.length)];
-        }, 10000);
+        }, 7500);
     });
 
     return (
         <div>
-            <p id="quote" className="text-2xl">
-                It's a beautiful day to leave me alone.
-            </p>
+            <p id="quote" className="text-2xl"></p>
         </div>
     );
 }
